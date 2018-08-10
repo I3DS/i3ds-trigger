47ae9c2cf3a9c31f0bb83420a125bf39a8364f51
Sigurd M. Albrektsen <sigurd.albrektsen@sintef.no>
2018-08-10 13:53:57 +0200

    Added emulator option to trigger client

--- src/i3ds_trigger.cpp
+++ src/i3ds_trigger.cpp
@@ -19,6 +19,7 @@
 namespace po = boost::program_options;
 namespace logging = boost::log;
 
+
 namespace i3ds
 {
 
@@ -27,12 +28,17 @@ class PetalinuxTrigger : public Trigger
 public:
 
   typedef std::shared_ptr<PetalinuxTrigger> Ptr;
-  static Ptr Create(Context::Ptr context, NodeID id)
+  static Ptr Create(NodeID id, bool emulated)
   {
-    return std::make_shared<PetalinuxTrigger>(context, id);
+    return std::make_shared<PetalinuxTrigger>(id, emulated);
   }
 
-  PetalinuxTrigger(Context::Ptr context, NodeID sensor);
+  PetalinuxTrigger(NodeID sensor, bool emulated) : Trigger(sensor) {
+    emulated_ = emulated;
+  }
+
+private:
+  bool emulated_;
 
 protected:
 
@@ -48,61 +54,76 @@ protected:
     TRIGGER_ID chan_id = static_cast<TRIGGER_ID>(command.request.channel);
     GENERATOR_ID gen_id = static_cast<GENERATOR_ID>(command.request.source);
     bool autostart = false;
-    std::cout << "Attempting to configure channel " << chan_id << ": gen " << gen_id << ", offset " << command.request.offset << ", duration " << command.request.duration << ", invert " << command.request.invert  << std::endl;
+    BOOST_LOG_TRIVIAL(info) << "Attempting to configure channel " << chan_id << ": gen " << gen_id << ", offset " << command.request.offset << ", duration " << command.request.duration << ", invert " << command.request.invert;
+    if (emulated_) {
+      BOOST_LOG_TRIVIAL(warning) << "  but not really since system is emulated.";
+      return;
+    }
     trigger_configure(chan_id, gen_id, command.request.offset, command.request.duration, command.request.invert, autostart);
   }
 
   // Handler for trigger external channel command, must be overloaded.
   void handle_external_channel(ExternalChannelService::Data& command) {
-    std::cout << "Handle external channel (not implemented)" << std::endl;
+    BOOST_LOG_TRIVIAL(error) << "Handle external channel not implemented";
     //TODO (sigurdm): implement
+    if (emulated_) {
+      BOOST_LOG_TRIVIAL(warning) << "  System is emulated.";
+      return;
+    }
   }
 
   // Handler for channel enable command, must be overloaded.
   void handle_enable_channel(ChannelEnableService::Data& command) {
-    std::cout << "Enabling channels:" << std::endl;
+    BOOST_LOG_TRIVIAL(info) <<  "Enabling channels:";
     //TODO (sigurdm): implement array/mask version?
     for (int i=0; i<8; i++) {
       if (command.request.arr[i]) {
 	TRIGGER_ID chan_id = static_cast<TRIGGER_ID>(i+1);
-	std::cout << "  Channel " << chan_id << std::endl;
-	trigger_enable(chan_id);
+	BOOST_LOG_TRIVIAL(info) <<  "  Channel " << chan_id;
+	if (!emulated_) {
+	  trigger_enable(chan_id);
+	}
       }
+    }
+    if (emulated_) {
+      BOOST_LOG_TRIVIAL(warning) << "  but not really, since system is emulated.";
     }
   }
 
   // Handler for channel disable command, must be overloaded.
   void handle_disable_channel(ChannelDisableService::Data& command) {
-    std::cout << "Disabling channels:" << std::endl;
+    BOOST_LOG_TRIVIAL(info) << "Disabling channels:";
     //TODO (sigurdm): implement array/mask version?
     for (int i=0; i<8; i++) {
       if (command.request.arr[i]) {
 	TRIGGER_ID chan_id = static_cast<TRIGGER_ID>(i + 1);
-	std::cout << "  Channel " << chan_id  << std::endl;
-	trigger_disable(chan_id);
+	BOOST_LOG_TRIVIAL(info) << "  Channel " << chan_id;
+	if (!emulated_) {
+	  trigger_disable(chan_id);
+	}
       }
+    }
+    if (emulated_) {
+      BOOST_LOG_TRIVIAL(warning) << "  but not really, since system is emulated.";
     }
   }
 
 };
 }
 
-i3ds::PetalinuxTrigger::PetalinuxTrigger(Context::Ptr context, NodeID sensor)
-  : Trigger(sensor)
-{
-}
-
 int main(int argc, char* argv[])
 {
-  NodeID node_id = atoi(argv[1]);
+  NodeID node_id;
   std::string addr_server;
 
   po::options_description desc("Driver for the trigger system");
+  bool emulated = false;
 
   desc.add_options()
   ("help", "Show help")
   ("node,n", po::value(&node_id)->default_value(300), "NodeID")
   ("addr-server,as", po::value(&addr_server)->default_value(""), "Address to the address-server")
+  ("emulated", po::bool_switch(&emulated)->default_value(false), "Emulate the trigger connection.")
   ;
 
   po::variables_map vm;
@@ -115,7 +136,11 @@ int main(int argc, char* argv[])
   }
 
   BOOST_LOG_TRIVIAL(info) << "Initializing trigger system.";
-  trigger_initialize();
+  if (!emulated) {
+    trigger_initialize();
+  } else {
+    BOOST_LOG_TRIVIAL(warning) << "WARNING - Using emulated system - WARNING";
+  }
 
   i3ds::Context::Ptr context;
   if (addr_server.empty()) {
@@ -125,7 +150,7 @@ int main(int argc, char* argv[])
   }
 
   BOOST_LOG_TRIVIAL(info) << "Creating trigger node with id: " << node_id;
-  i3ds::PetalinuxTrigger::Ptr trigger = i3ds::PetalinuxTrigger::Create(context, node_id);
+  i3ds::PetalinuxTrigger::Ptr trigger = i3ds::PetalinuxTrigger::Create(node_id, emulated);
   i3ds::Server server(context);
   BOOST_LOG_TRIVIAL(info) << "Attaching server.";
   trigger->Attach(server);
@@ -138,6 +163,8 @@ int main(int argc, char* argv[])
   }
 
   server.Stop();
-  trigger_deinitialize();
+  if (!emulated) {
+    trigger_deinitialize();
+  }
   return 0;
 }
